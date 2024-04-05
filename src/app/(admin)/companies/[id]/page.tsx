ffcab0c43a2abf8885e54619fc9dc30bb9c7a48e
Oleksii Khomiakov <alex.xag1987@gmail.com>
2024-04-05 09:04:22 +0300

    Layout in Next.js

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -2,7 +2,7 @@ import Header from '@/app/components/header';
 import React from 'react';
 
 export interface PageProps {
-  params: { id: string[] };
+  params: { id: string };
 }
 
 export default function Page({ params }: PageProps) {
